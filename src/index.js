f73bdef2f74bc40d1bf030160fa0901ff66826fb
Sanjeev <sksanjeev362@gmail.com>
2025-04-24 11:49:00 +0530

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client';
 // import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import Capture from './Capture';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
